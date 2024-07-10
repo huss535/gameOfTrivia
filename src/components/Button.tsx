ad7ce66c29aa74c821561d67b20a542280ca56ad
efar716 <110000165+efar716@users.noreply.github.com>
2024-07-10 13:29:12 +1200

    Button created

---
+++ src/components/Button.tsx
@@ -0,0 +1,19 @@
+interface ButtonProps {
+    buttonTitle: string,
+    eventHandler: React.MouseEventHandler<HTMLButtonElement>,
+}
+
+
+// Button component for web app
+function Button(props: ButtonProps) {
+
+
+    return (<button onClick={props.eventHandler}>{props.buttonTitle}</button>);
+
+
+
+
+}
+
+
+export default Button;
---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,12 @@
+function HomePage() {
+
+    return (
+        <div>
+            <h1>Game of trivia</h1>
+            <button style={{ backgroundColor: 'red' }}>Create Game</button>
+            <button >Join Game </button>
+
+        </div>);
+}
+
+export default HomePage;
