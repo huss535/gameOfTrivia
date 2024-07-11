89d7894eb2cb902cbf666fab72b974e1ccc21de9
efar716 <110000165+efar716@users.noreply.github.com>
2024-07-11 23:51:33 +1200

    Custom checkbox

---
+++ src/pages/CreateNewGame.tsx
@@ -0,0 +1,14 @@
+import Button from "../components/Button";
+
+function CreateNewGame() {
+
+    return (
+        <>
+
+            <Button buttonTitle="Next" eventHandler={() => { }} />
+        </>
+
+    );
+}
+
+export default CreateNewGame;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,24 @@
+import Button from "../components/Button";
+
+function HomePage() {
+
+    return (
+        <div>
+            <h1>Game of trivia</h1>
+            <Button buttonTitle="Join Game" eventHandler={() => { }} />
+            <Button buttonTitle="Create Game" eventHandler={() => { }} />
+            <div className="checkbox-container">
+                <div id="checkBox">
+                    <label >
+
+                        <input type="checkbox" />
+                        <span>Horror</span>
+
+                    </label>
+                </div>
+            </div>
+
+        </div>);
+}
+
+export default HomePage;
