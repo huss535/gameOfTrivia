65609b535b60582c0a81d12455e4bf3d265d8390
efar716 <110000165+efar716@users.noreply.github.com>
2024-07-22 07:37:20 +1200

    endpoint for questions

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,15 +17,16 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Hello, TypeScript with Express!');
 });
 
-
+//endpoint that handles generating the questions through Gemini API
 app.post("/fetchQuestions", async (req: Request, res: Response) => {
     const { topics } = req.body;
-    const prompt = "Please provide 20 fun trivia question of moderate difficulty about:[House of the dragon,Game of thrones,The walking dead] \n provide the questions followed by four answers of which only one is true the rmaining three are false \n Here is a sample for what the response should be: (What is the capital of Egypt-Cairo-paris-london-alexandria, and the following qestions follow the same form)"
+    // console.log(topics["categoryList"])
+    const prompt = `Please provide 20 fun trivia question of moderate difficulty about:${topics["categoryList"]} \n provide the questions followed by four answers of which only one is true the rmaining three are false \n Here is a sample for what the response should be: (What is the capital of Egypt-Cairo-paris-london-alexandria, and the following qestions follow the same form)`
 
     const result = await model.generateContent(prompt);
     const response = result.response;
     const text = response.text();
-    //console.log(text);
+    console.log(text);
     res.send(text)
 
 });
